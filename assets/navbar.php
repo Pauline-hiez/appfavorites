0212b0eedd5523d49aa0e239dfa866aa7c0c9762
Pauline-hiez <pauline.hiez@laplateforme.io>
2025-10-04 10:37:58 +0200

    carousel

---
+++ assets/navbar.php
@@ -0,0 +1,34 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Bienvenue</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+ 
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="index.php">Accueil</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="youtube.php">Youtube</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="canal.php">Canal plus</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="whatsapp.php">Profil</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="autres-app.php">Autres app</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="contact.php">Contact</a>
+        </li>
+         
+      </ul>
+    </div>
+  </div>
+</nav>
---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css">
+    <title>Accueil</title>
+</head>
+<body>
+
+<?php require_once "assets/header.php" ; ?>
+<?php require_once "assets/navbar.php"; ?>
+
+<main>
+
+<div id="carouselExampleDark" class="carousel carousel-dark slide">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active slide" data-bs-interval="10000">
+      <img src="assets/img/youtube.jpg" class="d-block w-100" alt="youtube">
+      <div class="carousel-caption d-none d-md-block">
+        <div class="bloc-text">
+        <h5>Youtube</h5>
+        <p>L'application de vidéos.</p>
+      </div>
+    </div>
+    </div>
+    <div class="carousel-item slide" data-bs-interval="2000">
+      <img src="assets/img/canal.webp" class="d-block w-100" alt="canal">
+      <div class="carousel-caption d-none d-md-block">
+        <div class="bloc-text">
+        <h5>Canal plus</h5>
+        <p>Grand choix de contenus : Films, séries, documentaires et même replay et tv en direct.</p>
+      </div>
+      </div>
+    </div>
+    <div class="carousel-item slide">
+      <img src="assets/img/whatsapp.jpg" class="d-block w-100" alt=".whatsapp">
+      <div class="carousel-caption d-none d-md-block">
+        <div class="bloc-text">
+        <h5>WhatsApp</h5>
+        <p>L'application pour communiquer avec votre famille et vos amis, partout dans le monde.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+  
+</main>
+
+<?php require_once "assets/footer.php" ; ?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>

ad8e9248ca924d2167f968601ada52fc78bcf354
Pauline-hiez <pauline.hiez@laplateforme.io>
2025-10-05 11:53:44 +0200

    youtube-en-cours

---
+++ youtube.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/style.css">
+    <title>YouTube - Applications Favorites</title>
+</head>
+<body>
+
+<?php require_once "assets/header.php" ?>
+<?php require_once "assets/navbar.php" ?>
+    
+<div class="container-fluid text-white">
+    <div class="row align-items-center mb-4">
+        <div class="col-lg-6 col-md-12 mb-3 mb-lg-0 text-center">
+            <img src="assets/img/youtube.webp" class="img-fluid rounded shadow-sm" style="max-width: 600px; width: 100%;" alt="youtube">
+        </div>
+        <div class="col-lg-6 col-md-12 ps-lg-4">
+            <h3 class="text-white fw-bold mb-3 text-center text-lg-start">Youtube</h3>
+            <p class="text-light mb-3">Téléchargez l'application YouTube officielle sur votre téléphone ou tablette Android. Découvrez les contenus regardés partout dans le monde : des clips musicaux du moment aux vidéos populaires sur les jeux vidéo, la mode, la beauté, les actualités, l'éducation et bien plus encore. Abonnez-vous aux chaînes que vous aimez, créez vos propres vidéos, partagez des contenus avec vos amis et profitez de YouTube sur n'importe quel appareil.</p>
+            <p class="text-light fw-semibold">Regardez des vidéos et abonnez-vous à des chaînes</p>
+        </div>
+    </div>
+    <div class="ligne2">
+    <div class="bloc-text-2"></div>
+        <h3 class="text-white fw-bold">Avis clients</h3>
+            <p class="text-warning fw-bold">darkos zertox ★★★★★</p>
+            <p class="text-light">Très bonne appli que j'utilise depuis très longtemps permettant au créateur de contenu de publier les vidéos, short ou annonce. qui permet au divertissement et qui m'a beaucoup servi pour simplement passer le temps. Malgré qu'il y ait des pubs je trouve que sur mobile elles ne sont pas gênante même si lorsque vous utilisez votre télé connecté le nombre de pubs et abusé et elle dur 1 min chacune un enfer si vous souhaitez regarder sur télé</p>
+
+            <p class="text-warning fw-bold">Yvy Kenfack ★★★★★</p>
+            <p class="text-light">Très belle Appli' car assez fonctionnelle . Et la variété des contenus proposés est satisfaisante . Je m'y instruits à travers les expériences des uns et des autres . Et je m'y distrait aussi . Bref , depuis plus de 5 ans que j'utilise cette Appli ' j'suis toujours satisfaite. </p>
+
+            <p class="text-warning fw-bold">Galéana Fernandes ★★★★☆</p>
+            <p class="text-light">Bonsoir Youtube ! j'ai rencontré un grand bug. En faite tous un coup quand on vas dans "historique" dans l'application, c'est vide et il y a seulement les shorts regardé. j'ai essayé d'appuyer sur l'onglet "musique" et ça affiche tout mais sans la ligne rouge (la ligne de lecture). Merci de bien corrigé ce bug. j'ai essayé de redémarrer mon téléphone (2 fois) ça persiste. Mais j'espère que mon commentaire était clair. Au revoir</p>
+    </div>
+</div>
+
+
+<?php require_once "assets/footer.php" ?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
